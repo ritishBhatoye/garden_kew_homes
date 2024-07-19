f6bee3196601d98bd6b34a19fa79e4cebd0a2c5a
Simransingh010 <simran.ahuja0010@gmail.com>
2024-07-20 02:00:46 +0530

    added images

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Carousel } from 'react-responsive-carousel'
 import CarouselComponent from '../components/landingPage/Carousel'
 import ImageCarousel from '../components/landingPage/ImageCarousel'
+import TestimonialsSection from '../components/landingPage/testimonial'
 
 const LandingPage = () => {
   return (
     <div>
      <CarouselComponent/>
      <ImageCarousel/>
+     <TestimonialsSection/>
     </div>
   )
 }
